be55d3638bf2fd692044d7470c2021ce2a3446eb
MaTtiaa01 <mattia2001@live.it>
2023-07-17 15:35:48 +0200

    add config

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const blogRoutes = require('./routes/blogRoutes')
-const morgan = require('morgan')
+const express = require("express");
+const blogRoutes = require("./routes/blogRoutes");
+const morgan = require("morgan");
+require("dotenv").config();
 // const path = require('path')
 // const _ = require('lodash')
 
@@ -11,51 +12,50 @@ const app = express();
 // app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 // app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
-
-
 //connect to db
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const key = process.env.MONGO_DB;
 
-mongoose.connect('mongodb+srv://mattia2001:KswyCRd5Xrt9KyFq@cluster0.gumz4jb.mongodb.net/node-blogs')
-    .then((results) => {
-        app.listen('4000')
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+mongoose
+  .connect(
+    "mongodb+srv://mattia2001:" +
+      key +
+      "@cluster0.gumz4jb.mongodb.net/node-blogs"
+  )
+  .then((results) => {
+    app.listen("4000");
+    console.log("app connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-// set view engine
-app.set('view engine', 'ejs')
-
+//set view engine
+app.set("view engine", "ejs");
 
 //middleware
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 //to go on
 app.use((req, res, next) => {
-    res.locals.path = req.path
-    next()
-})
-
-
-
-//routes
-app.get('/', (req, res) => {
-    res.redirect('/blogs');
+  res.locals.path = req.path;
+  next();
 });
 
+//routes
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
 
-app.use('/blogs', blogRoutes)
+app.use("/blogs", blogRoutes);
 
-app.get('/about', (req, res) => {
-    res.render('about')
-})
-
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
 //404 page not found
 app.use((req, res) => {
-    res.status(404).render('404')
-})
-
+  res.status(404).render("404");
+});
